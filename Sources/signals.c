b55490f1f804d8a5d5e051d4b9d58734015a7f8d
Hugo Cremers <hcremers@student.s19.be>
2022-05-11 14:10:13 +0200

    Mini_Sheh v0.3.3 : Fonction défectueuse tue

--- Sources/signals.c
+++ Sources/signals.c
@@ -6,48 +6,24 @@
 /*   By: hcremers <hcremers@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 15:48:55 by hcremers          #+#    #+#             */
-/*   Updated: 2022/05/11 11:47:53 by hcremers         ###   ########.fr       */
+/*   Updated: 2022/05/11 14:05:47 by hcremers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/minishell.h"
-// #include "Mini_sheh/Includes/pipe.h"
 
 void	handler(int sig)
 {
-	if (sig == SIGINT)
-	{
-		rl_replace_line("", 0);
-		ft_putstr_fd("\n", 1);
-		rl_on_new_line();
-		rl_redisplay();
-		// g_datas.last_command_status = 1;
-	}
+	(void)sig;
+	// rl_replace_line("", 0);
+	ft_putstr_fd("\n", 1);
+	rl_on_new_line();
+	rl_redisplay();
+	// g_datas.last_command_status = 1;
 }
 
 void	ft_signal(void)
 {
 	signal(SIGINT, handler);
-	signal(SIGQUIT, handler);
-	// signal(SIGUSR1, handler);
-	// signal(SIGUSR2, handler);
+	signal(SIGQUIT, SIG_IGN);
 }
-
-// int	main(void)
-// {
-// 	pid_t	pid;
-// 	int		sig;
-
-// 	pid = getpid();
-// 	printf("%d\n", pid);
-// 	ft_signal();
-// 	sig = -1;
-// 	while (sig)
-// 	{
-// 		scanf("%d", &sig);
-// 		if (sig == 1)
-// 			kill(pid, SIGUSR1);
-// 		if (sig == 2)
-// 			kill(pid, SIGUSR2);
-// 	}
-// }
