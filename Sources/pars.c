058f484b5c20c88394b25b5e2b3f5bfb69ed8138
Alan Cailleau <acaillea@mi-r2-p1.s19.be>
2022-05-30 16:08:33 +0200

    meh

--- Sources/pars.c
+++ Sources/pars.c
@@ -148,7 +148,7 @@ t_one	*ft_pars(char const *s, t_data *d)
 	}
 	d->all->nb_cmd = i;
 	d->all->first = init_tmp;/////// utile ?
-	// if (init_cmds(d)) // envoit vers le dossier pars_end.c
-	// 	return (NULL);
+	if (init_cmds(d)) // envoit vers le dossier pars_end.c
+		return (NULL);
 	return (d->all->first);
 }
--- Sources/pars_end.c
+++ Sources/pars_end.c
@@ -12,124 +12,124 @@
 
 #include "../Includes/minishell.h"
 
-// check env_var avec lststr
-// char	*env_or_not_env(char *str, t_data *d)
-// {
-// 	t_list	*res;
-// 	char	*ret;
+check env_var avec lststr
+char	*env_or_not_env(char *str, t_data *d)
+{
+	t_list	*res;
+	char	*ret;
 
-// 	res = ft_lststr(d->env_list, str);
-// 	if (res)
-// 	{
-// 		ret = (char *)malloc(sizeof(char) * (ft_strlen(res->content + 1)));
-// 		if (!ret)
-// 			return (NULL);
-// 		ft_strlcpy(ret, res->content, ft_strlen(res->content));
-// 		return (ret);
-// 	}
-// 	return (NULL);
-// }
+	res = ft_lststr(d->env_list, str);
+	if (res)
+	{
+		ret = (char *)malloc(sizeof(char) * (ft_strlen(res->content + 1)));
+		if (!ret)
+			return (NULL);
+		ft_strlcpy(ret, res->content, ft_strlen(res->content));
+		return (ret);
+	}
+	return (NULL);
+}
 
-// check env_var sans lststr
-// char	*env_or_not_env(char *str, t_data *d)
-// {
-// 	t_list	*tmp;
-// 	char	*ret;
+check env_var sans lststr
+char	*env_or_not_env(char *str, t_data *d)
+{
+	t_list	*tmp;
+	char	*ret;
 
-// 	tmp = d->env_list;
-// 	while (d->env_list)
-// 	{
-// 		if (ft_strncmp(d->env_list->name, str, ft_strlen(d->env_list->name)))
-// 		{
-// 			ret = (char *)malloc(sizeof(char) * (ft_strlen(d->env_list->name) + 1));
-// 			if (!ret)
-// 				return (NULL);
-// 			ft_strlcpy(ret, d->env_list->content, ft_strlen(d->env_list->content));
-// 			printf("%s\n", str);
-// 			return (ret);
-// 		}
-// 		d->env_list = d->env_list->next;
-// 	}
-// 	d->env_list = tmp;
-// 	return (str);
-// }
+	tmp = d->env_list;
+	while (d->env_list)
+	{
+		if (ft_strncmp(d->env_list->name, str, ft_strlen(d->env_list->name)))
+		{
+			ret = (char *)malloc(sizeof(char) * (ft_strlen(d->env_list->name) + 1));
+			if (!ret)
+				return (NULL);
+			ft_strlcpy(ret, d->env_list->content, ft_strlen(d->env_list->content));
+			printf("%s\n", str);
+			return (ret);
+		}
+		d->env_list = d->env_list->next;
+	}
+	d->env_list = tmp;
+	return (str);
+}
 
-// char	*join_mat(char **tab)
-// {
-// 	int		i;
-// 	char	*str;
+char	*join_mat(char **tab)
+{
+	int		i;
+	char	*str;
 
-// 	i = 0;
-// 	while (tab && tab[i])
-// 	{
-// 		str = ft_strjoin(str, tab[i]);
-// 		if (!str)
-// 			return (NULL);
-// 		free(tab[i]);
-// 		i++;
-// 	}
-// 	return (str);
-// }
+	i = 0;
+	while (tab && tab[i])
+	{
+		str = ft_strjoin(str, tab[i]);
+		if (!str)
+			return (NULL);
+		free(tab[i]);
+		i++;
+	}
+	return (str);
+}
 
-// int	check_env_var(char *str, t_data *d)
-// {
-// 	int		j;
-// 	int		k;
-// 	int		t;
-// 	char	**tmp_tab;
+int	check_env_var(char *str, t_data *d)
+{
+	int		j;
+	int		k;
+	int		t;
+	char	**tmp_tab;
 
-// 	tmp_tab = (char **)malloc(sizeof(char *) * 4);
-// 	if (!tmp_tab)
-// 		return (-19);
-// 	j = check_c_in(str, '$');
-// 	if (j)
-// 	{
-// 		k = j;
-// 		t = 0;
-// 		while (str[j] && str[j] != '$' && str[j] != 9 && str[j] != 32)
-// 			j++;
-// 		tmp_tab[t] = ft_substr(str, 0, k - 1);
-// 		if (!tmp_tab[t])
-// 			return (-19);
-// 		tmp_tab[++t] = env_or_not_env(str, d);
-// 		if (!tmp_tab[t])
-// 			return (-19);
-// 		tmp_tab[++t] = ft_substr(str, j, ft_strlen(str));
-// 		if (!tmp_tab[t])
-// 			return (-19);
-// 		str = join_mat(tmp_tab);
-// 		if (!str)
-// 			return (-19);
-// 	}
-// 	free(tmp_tab);
-// 	return (0);
-// }
+	tmp_tab = (char **)malloc(sizeof(char *) * 4);
+	if (!tmp_tab)
+		return (-19);
+	j = check_c_in(str, '$');
+	if (j)
+	{
+		k = j;
+		t = 0;
+		while (str[j] && str[j] != '$' && str[j] != 9 && str[j] != 32)
+			j++;
+		tmp_tab[t] = ft_substr(str, 0, k - 1);
+		if (!tmp_tab[t])
+			return (-19);
+		tmp_tab[++t] = env_or_not_env(str, d);
+		if (!tmp_tab[t])
+			return (-19);
+		tmp_tab[++t] = ft_substr(str, j, ft_strlen(str));
+		if (!tmp_tab[t])
+			return (-19);
+		str = join_mat(tmp_tab);
+		if (!str)
+			return (-19);
+	}
+	free(tmp_tab);
+	return (0);
+}
 
-// int	init_cmds(t_data *d)
-// {
-// 	int	i;
-// 	int	j;
-// 	t_one	*tmp;
+int	init_cmds(t_data *d)
+{
+	int	i;
+	int	j;
+	t_one	*tmp;
 
-// 	i = 0;
-// 	tmp = d->all->first;
-// 	while (d->all->first)
-// 	{
-// 		d->all->first->pos = d->all->nb_cmd - i - 1;
-// 		j = 0;
-// 		while (d->all->first->pars_tab[j])
-// 		{//check_in_c renvoit la position du premier $ dans la string
-// 			if ((check_c_in(d->all->first->pars_tab[j], '$') && d->all->first->pars_tab[j][0] == '\"')
-// 				 || (check_c_in(d->all->first->pars_tab[j], '$') && d->all->first->pars_tab[j][0] != '\''))
-// 			{
-// 				if (check_env_var(d->all->first->pars_tab[j], d))
-// 					return (-19);
-// 			}
-// 			j++;
-// 		}
-// 		d->all->first = d->all->first->next;
-// 		i++;
-// 	}
-// 	d->all->first = tmp;
-// 	return (0);
-// }
+	i = 0;
+	tmp = d->all->first;
+	while (d->all->first)
+	{
+		d->all->first->pos = d->all->nb_cmd - i - 1;
+		j = 0;
+		while (d->all->first->pars_tab[j])
+		{//check_in_c renvoit la position du premier $ dans la string
+			if ((check_c_in(d->all->first->pars_tab[j], '$') && d->all->first->pars_tab[j][0] == '\"')
+				 || (check_c_in(d->all->first->pars_tab[j], '$') && d->all->first->pars_tab[j][0] != '\''))
+			{
+				if (check_env_var(d->all->first->pars_tab[j], d))
+					return (-19);
+			}
+			j++;
+		}
+		d->all->first = d->all->first->next;
+		i++;
+	}
+	d->all->first = tmp;
+	return (0);
+}
